47938784416d762024d19e315984ec0dbdf6d75d
Victor Winberg <viwi@netcompany.com>
2020-02-04 13:12:56 +0100

    Updates

---
+++ precache-manifest.8466417fc3ba295f3b81765bb896e9ed.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41857564dfa04f5c46ddf5f16f998417",
+    "url": "/ces/index.html"
+  },
+  {
+    "revision": "5905c2a7baabc3efedef",
+    "url": "/ces/static/css/2.70d9d258.chunk.css"
+  },
+  {
+    "revision": "199533db51ec317e7448",
+    "url": "/ces/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5905c2a7baabc3efedef",
+    "url": "/ces/static/js/2.2f2be662.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/ces/static/js/2.2f2be662.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "199533db51ec317e7448",
+    "url": "/ces/static/js/main.75b773e2.chunk.js"
+  },
+  {
+    "revision": "af137e5905d52a7eccde",
+    "url": "/ces/static/js/runtime-main.9b4084b0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ces/static/media/logo.5d5d9eef.svg"
+  }
+]);
